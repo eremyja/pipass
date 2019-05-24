552d523d02834aa07b1a508d33cbc256134234a2
Dalton Wilson <eremyja@gmail.com>
2019-05-23 16:19:37 -1000

    break out display set up

---
+++ setup_display.py
@@ -0,0 +1,29 @@
+###
+# Requires:
+import board
+import busio
+import adafruit_ssd1306
+###
+
+def setup_display() :
+    # Create the I2C interface.
+    i2c = busio.I2C(board.SCL, board.SDA)
+    # Create the SSD1306 OLED class.
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+
+    # Clear display.
+    disp.fill(0)
+    disp.show()
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
