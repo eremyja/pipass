2eed0580da42ccfe5e6be0c812de1b976175e2f1
Dalton Wilson <eremyja@gmail.com>
2019-05-25 00:01:04 -1000

    .

--- text_display.py
+++ text_display.py
@@ -101,7 +101,7 @@ def text_input_test(device, draw, font, wrap_text) : #, passfrase, c_c_c_combo, 
             c_c_c_combo.append("B")
             buttons_pressed = buttons_pressed + "*"
             time.sleep(.2)
-        else
+        else :
             return True
 #        if not button_U.value :
 #            wrap_text("Up")
