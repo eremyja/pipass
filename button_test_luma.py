8c6c99d0c8ad0ec9721140a83795173c4860f6ad
Dalton Wilson <eremyja@gmail.com>
2019-05-24 21:20:14 -0400

    fix

--- button_test_luma.py
+++ button_test_luma.py
@@ -11,37 +11,39 @@ device = ssd1306(serial)
 
 with canvas(device) as draw:
     while True:
-        if button_U.value: # button is released
-            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  #Up
-        else: # button is pressed:
-            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  #Up filled
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((30, 40), "Hello World", fill="white")
+#        if button_U.value: # button is released
+#            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  #Up
+#        else: # button is pressed:
+#            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)  #Up filled
 
-        if button_L.value: # button is released
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left
-        else: # button is pressed:
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=1)  #left filled
+#        if button_L.value: # button is released
+#            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left
+#        else: # button is pressed:
+#            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=1)  #left filled
 
-        if button_R.value: # button is released
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right
-        else: # button is pressed:
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=1) #right filled
+#        if button_R.value: # button is released
+#            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right
+#        else: # button is pressed:
+#            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=1) #right filled
 
-        if button_D.value: # button is released
-            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0) #down
-        else: # button is pressed:
-            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=1) #down filled
+#        if button_D.value: # button is released
+#            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0) #down
+#        else: # button is pressed:
+#            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=1) #down filled
 
-        if button_C.value: # button is released
-            draw.rectangle((20, 22, 40, 40), outline=255, fill=0) #center
-        else: # button is pressed:
-            draw.rectangle((20, 22, 40, 40), outline=255, fill=1) #center filled
+#        if button_C.value: # button is released
+#            draw.rectangle((20, 22, 40, 40), outline=255, fill=0) #center
+#        else: # button is pressed:
+#            draw.rectangle((20, 22, 40, 40), outline=255, fill=1) #center filled
 
-        if button_A.value: # button is released
-            draw.ellipse((70, 40, 90, 60), outline=255, fill=0) #A button
-        else: # button is pressed:
-            draw.ellipse((70, 40, 90, 60), outline=255, fill=1) #A button filled
+#        if button_A.value: # button is released
+#            draw.ellipse((70, 40, 90, 60), outline=255, fill=0) #A button
+#        else: # button is pressed:
+#            draw.ellipse((70, 40, 90, 60), outline=255, fill=1) #A button filled
 
-        if button_B.value: # button is released
-            draw.ellipse((100, 20, 120, 40), outline=255, fill=0) #B button
-        else: # button is pressed:
-            draw.ellipse((100, 20, 120, 40), outline=255, fill=1) #B button filled
+#        if button_B.value: # button is released
+#            draw.ellipse((100, 20, 120, 40), outline=255, fill=0) #B button
+#        else: # button is pressed:
+#            draw.ellipse((100, 20, 120, 40), outline=255, fill=1) #B button filled
