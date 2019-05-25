c70b046d95bb63cb3b31c40e8610ebf7fd6b42a0
Dalton Wilson <eremyja@gmail.com>
2019-05-24 21:03:04 -0400

    fix

--- display_init_luma.py
+++ display_init_luma.py
@@ -5,5 +5,5 @@ from luma.oled.device import ssd1306
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
 
-with canvas(device) as draw:
+#with canvas(device) as draw:
 #    draw.rectangle(device.bounding_box, outline="white", fill="black")
