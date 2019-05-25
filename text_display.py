ec2182d91999bfab9d63bdc20576d451911ddd9a
Dalton Wilson <eremyja@gmail.com>
2019-05-24 16:17:33 -1000

    fix

--- text_display.py
+++ text_display.py
@@ -20,26 +20,30 @@ def make_font(name, size) :
     font_path = os.path.abspath(os.path.join(
         os.path.dirname(__file__), 'fonts', name))
     return ImageFont.truetype(font_path, size)
-def main(device) :
-    while True:
+def text_input_test(device) :
+    #while True:
         #for fontname, size in [(None, None), ("tiny.ttf", 6), ("ProggyTiny.ttf", 16), ("creep.bdf", 16), ("miscfs_.ttf", 12), ("FreePixel.ttf", 12), ('ChiKareGo.ttf', 16)]:
-            font = make_font(fontname, fontsize) if fontname else None
+    font = make_font(fontname, fontsize) if fontname else None
             #term = terminal(device, font)
 
-            if not button_U.value :
-                draw.text((0,0), "Pressing Up", font=font, fill="white")
-            elif not button_D.value :
-                draw.text((0,0), "Pressing Down", font=font, fill="white")
-            elif not button_L.value :
-                draw.text((0,0), "Pressing Left", font=font, fill="white")
-            elif not button_R.value :
-                draw.text((0,0), "Pressing Right", font=font, fill="white")
-            elif not button_A.value :
-                draw.text((0,0), "Pressing A", font=font, fill="white")
-            elif not button_B.value :
-                draw.text((0,0), "Pressing B", font=font, fill="white")
-            else :
-                draw.text((0,0), "Waiting for you to press something", font=font, fill="white")
+    if not button_U.value :
+        draw.text((0,0), "Pressing Up", font=font, fill="white")
+    elif not button_D.value :
+        draw.text((0,0), "Pressing Down", font=font, fill="white")
+    elif not button_L.value :
+        draw.text((0,0), "Pressing Left", font=font, fill="white")
+    elif not button_R.value :
+        draw.text((0,0), "Pressing Right", font=font, fill="white")
+    elif not button_A.value :
+        draw.text((0,0), "Pressing A", font=font, fill="white")
+    elif not button_B.value :
+        draw.text((0,0), "Pressing B", font=font, fill="white")
+    else :
+        draw.text((0,0), "Waiting for you to press something", font=font, fill="white")
+
+def main() :
+    while True :
+        text_input_test(device)
 
 if __name__ == "__main__":
     try:
